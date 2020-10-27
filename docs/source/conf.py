44cf88707756ba220ce1721b5ce077bc4926ae4d
Stanley Phoong <stanley.cheong.kwan.phoong@intel.com>
2020-10-28 02:01:18 +0800

    Fixed the relative path for doxygen
    Signed-off-by: Stanley Phoong <stanley.cheong.kwan.phoong@intel.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -73,7 +73,7 @@ exhale_args = {
     # TIP: if using the sphinx-bootstrap-theme, you need
     # "treeViewIsBootstrap": True,
     "exhaleExecutesDoxygen": True,
-    "exhaleDoxygenStdin":    "INPUT = ../include"
+    "exhaleDoxygenStdin":    "INPUT = ../../include"
 }
 
 # Tell sphinx what the primary language being documented is.
