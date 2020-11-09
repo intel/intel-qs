50a51810afd92d1a52ee384d8723d9ef2c4bc290
giangiac <gian.giacomo.guerreschi@intel.com>
2020-11-09 13:55:52 -0800

    Update communication_reduction_via_qubit_reordering.cpp

--- examples/communication_reduction_via_qubit_reordering.cpp
+++ examples/communication_reduction_via_qubit_reordering.cpp
@@ -128,6 +128,12 @@ int main(int argc, char **argv)
   end =  time.tv_sec + time.tv_usec * 1.0e-6;
   if (myrank==0)
       std::cout << "inverse qubit order --> Simulation time = " << end-start << "\n";
-             
+    
+  // Verify that the final state is the same.
+  psi_inverse_order.PermuteQubits(original_map, "direct");
+  double overlap_sq = std::norm(psi_trivial_order.ComputeOverlap(psi_inverse_order));
+  if (myrank==0)
+      std::cout << "Squared overlap of states at the end of the two simulations = " << overlap_sq << "\n\n";
+         
   return 0;
 }
